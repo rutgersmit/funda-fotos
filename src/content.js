f6646c7ddfe206e61f18fbdd3d685d22e724e3cc
Rutger Smit <r.smit1@windesheim.nl>
2023-06-26 21:29:57 +0200

    tidying up things before submitting

--- src/content.js
+++ src/content.js
@@ -24,33 +24,37 @@ function parseHouse(domRef, data) {
     .forEach(function (img) {
       // skip the 360 and floorplan images
       let alt = img.getAttribute("alt").toLowerCase();
-      if (alt.indexOf("360") >=0 || alt.indexOf("plattegrond") >= 0) return;
+      if (alt.indexOf("360") >= 0 || alt.indexOf("plattegrond") >= 0) return;
 
       images.push(img.getAttribute("data-lazy"));
     });
 
-    let html = '<div style="width: 100%; overflow-x: auto;"><div style="display: flex;" onclick="navigator.clipboard.writeText(\'lalala\').then(() => {}, () => {});">';
-    images.forEach(function (img) {
-      html +=
-        "<a target='_blank' href='" +
-        img +
-        "' style='margin: 10px;'><img src='" +
-        img.replace("1440x960", "360x240") +
-        "' style='width: 360px; height: 240px; max-width: initial;'/></a>";
-    });
-    html += "</div></div>";
-    images = [];
+  // we skip the first element because it is on the page already
+  images.shift();
 
-    let div = document.createElement("div");
-    div.innerHTML = html;
-    domRef.parentElement.appendChild(div);
+  let html =
+    '<div style="width: 100%; overflow-x: auto;"><div style="display: flex;" onclick="navigator.clipboard.writeText(\'lalala\').then(() => {}, () => {});">';
+  images.forEach(function (img) {
+    html +=
+      "<a target='_blank' href='" +
+      img +
+      "' style='margin: 10px;'><img src='" +
+      img.replace("1440x960", "360x240") +
+      "' style='width: 360px; height: 240px; max-width: initial;'/></a>";
+  });
+  html += "</div></div>";
+  images = [];
+
+  let div = document.createElement("div");
+  div.innerHTML = html;
+  domRef.parentElement.appendChild(div);
 }
 
 
 // elem is the div element of the house
 function upgradeHouse(elem){
   let url = elem.querySelector('a[data-test-id="object-image-link"]').href;
-  console.log(url);
+  //console.log(url);
   //elem.querySelector('h2').innerHTML += " - check";
 
   httpGetAsync(url, elem, parseHouse);
