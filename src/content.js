3acbe880b3850c2552c616ea4c4da82303211c0a
Rutger Smit <rutger.smit@gmail.com>
2023-07-11 20:44:01 +0200

    refactoring and smooth logging

--- src/content.js
+++ src/content.js
@@ -1,21 +1,28 @@
+let LOG = true;
+
 // domRef is the div element of the house
-function httpGetAsync(theUrl, domRef, callback) {
-  console.log("httpGetAsync: " + theUrl);
+function httpGetAsync(houseId, theUrl, domRef, callback) {
+  log("[" + houseId + "] Get " + theUrl);
   //theUrl = 'https://www.funda.nl/koop/laren-nh/huis-42252648-mauvezand-20/';
   let xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function () {
-    console.log("url: " + theUrl + " - onreadystatechange: " + xmlHttp.readyState + " - " + xmlHttp.status);
+    //log("url: " + theUrl + " - onreadystatechange: " + xmlHttp.readyState + " - " + xmlHttp.status);
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-      parseHouse(domRef, xmlHttp.responseText);
+      parseHouse(houseId, domRef, xmlHttp.responseText);
     else if(xmlHttp.status == 404)
-      houseDoesNotExist(domRef);
+      houseDoesNotExist(houseId, domRef);
   };
+
   xmlHttp.open("GET", theUrl, true);
   xmlHttp.send(null);
 }
 
-function houseDoesNotExist(domRef) {
-  let html = '<div class="x" style="width: 100%; overflow-x: auto;"><div style="display: flex;">';
+
+function houseDoesNotExist(houseId, domRef) {
+  log("["+ houseId+"] House does not exist (yet)");
+
+  let html =
+    '<div class="x" style="width: 100%; overflow-x: auto;"><div style="display: flex;">';
   html += "Huis bestaat niet";
   html += "</div></div>";
 
@@ -26,12 +33,14 @@ function houseDoesNotExist(domRef) {
 
 
 // domRef is the div element of the house
-function parseHouse(domRef, data) {
-  //console.debug(domRef);
+function parseHouse(houseId, domRef, data) {
   if (domRef.parentElement.querySelectorAll('div[class="x"]').length > 0) {
     // some houses have already been upgraded, don't know why 🤷‍♀️
+    log("["+ houseId+"] Already upgraded");
     return;
   }
+
+  log("["+ houseId+"] Parse house data");
 
   // make an empty array to store the images
   let images = [];
@@ -50,12 +59,14 @@ function parseHouse(domRef, data) {
     });
 
   if (domRef.parentElement.querySelectorAll("header").length > 0) {
-    // we skip the first three because they are on the page already
+    // we skip the first three because they are on the page already (promoted houses)
     images = images.slice(3);
   } else {
     // we skip the first element because it is on the page already
     images.shift();
   }
+
+  log("[" + houseId + "] Adding " + images.length + " images");
 
   // for future use:  onclick="navigator.clipboard.writeText(\'lalala\').then(() => {}, () => {});"
   let html =
@@ -83,25 +94,36 @@ function parseHouse(domRef, data) {
 
 
 // elem is the div element of the house
-function upgradeHouse(elem){
+function upgradeHouse(houseId, elem) {
   //let querySelectorTag = 'div[class="search-result-media"]';
-  let querySelectorTag = 'a';
-  let url = elem.querySelector(querySelectorTag).href;
-  //console.log(url);
-  //elem.querySelector('h2').innerHTML += " - check";
 
-  httpGetAsync(url, elem, parseHouse);
+  let url = "https://www.funda.nl/" + houseId;
+
+  log("[" + houseId + "] Upgrade via " + url);
+  elem.querySelector('h2').innerHTML += " 🏳️";
+
+  httpGetAsync(houseId, url, elem, parseHouse);
 }
+
 
 function startUpgrading(){
   //let querySelectorTag = 'div[class="search-result-content"], div[class="search-result-content-promo"]';
   let querySelectorTag = 'div[data-test-id="search-result-item"]';
   let elems = document.querySelectorAll(querySelectorTag);
 
-  console.log(elems.length);
+  log(elems.length + " houses found");
   for (var i = 0; i < elems.length; i++) {
-    upgradeHouse(elems[i]);
+    let houseId = elems[i].querySelector('a').href.match(/huis-(\d+)/)[1];
+    log("[" + houseId + "] Found");
+    // from the variable url, print the number after the word 'huis-'
+    upgradeHouse(houseId, elems[i]);
   }
+}
+
+
+function log(message) {
+  if(LOG)
+    console.log(message);
 }
 
 
@@ -115,19 +137,17 @@ function debounce(func, timeout = 300) {
   };
 }
 
+
 const processChange = debounce(() => startUpgrading());
-//let querySelectorTag = 'div[data-component="pagination"]';
-let querySelectorTag =
-  'ul[class="pagination pagination-mobile"], div[id^="vue-portal-target"]';
+let querySelectorTag = 'ul[class="pagination pagination-mobile"]';
 
 new MutationObserver(function () {
-  console.log("Navigated");
+  log("Navigated");
   processChange();
 }).observe(document.querySelector(querySelectorTag), {
   childList: true,
   subtree: true,
   attributes: true
 });
-
 
 startUpgrading();
